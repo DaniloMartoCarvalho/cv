16728037fbd12a90d2b2192f0ce7bed600df7373
Danilo Marto de Carvalho <dmcarvalho.dmc@yandex.com>
2023-03-11 16:55:57 -0300

    Atualize a estrutura e o conteúdo do meu CV

--- datas.py
+++ datas.py
@@ -5,21 +5,18 @@ preencher o placeholders do template.
 
 datas = {
     "nome": "Danilo Marto de Carvalho",
-    "resumo": "Profissional com menos de 1 ano de experiência na área administrativa. Possui alguma noção  de python (linguagem de programação)  para uso em escritório. Também possui alguma noção de suitês de escritório das mais variadas origens.",
+    "resumo": "Profissional interessado em colaborar com atividades de escritório, fornecendo apoio administrativo aos colegas colaboradores",
     "data_de_nascimento": "12/01/1993",
     "status_civil": "solteiro",
     "telefone": "(12) 9 8321 6700",
     "email": "dmcarvalho.dmc@yandex.com",
-    "endereco": "R. Tomé de Souza, 335. Nova Guerá. Guaratinguetá. SP – 12515-590",
+    "endereco": "R. Tomé de Souza, 335. Nova Guará. Guaratinguetá-SP. 12515-590",
     "habilidades": [
-        "Suitês de escritótios (MS office, Libreoffice, Google Office, etc)",
-        "Python para tarefas de coleta e exploração de dados, automação e tarefas de cunho repetitivos, desenvolvimento web (em nível junior)",
-        "Noções de SQL",
-        "Familiaridade com ambiente Linux"
+        "Manipulação e tratamento de dados usando o python, SQL e planilhas gráficas",
+        "Confecção de documentos e relatórios usando python e ferramentas gráficas de escritótio",
+        "Familiaridade com ambiente Linux",
+        "inglês suficiente apenas para leitura",
     ],
-    "linguas_extrangeiras": {
-        "Inglês": "leio razoavelmente",
-    },
     "formacao_academica": [
         {
             "ano": "2012",
@@ -28,16 +25,10 @@ datas = {
             "status": "completo",
         },
         {
-            "ano": "2014-2018",
-            "curso": "Bacharelado em Física",
-            "instituicao": "UNESP, Campus de Guaratinguetá, SP",
-            "status": "incompleto",
-        },
-        {
-            "ano": "2018-2020",
-            "curso": "Análise e Desenvolvimento de Sistemas",
-            "instituicao": "FATEC Guaratinguetá, SP",
-            "status": "incompleto",
+            "ano": "2023-presente",
+            "curso": "Assistente de contabilidade",
+            "instituicao": "Gualifica Guará",
+            "status": "Cursando",
         },
     ],
     "experiencia_profissional": [
@@ -46,9 +37,10 @@ datas = {
             "cargo": "Aux. administrativo",
             "empresa": "Valetur Transportes Locação e Turismo LTDA",
             "atuacoes": [
-                "Responsável pela escala de fretamento, com tarefas de supervisionamento da prontidão dos motoristas colaboradores e da equipe de limpeza",
+                "Responsável por monitorar a prontidão dos motoristas colaboradores",
+                "Responsável pelo acompanhamento os trabalhos da equipe de limpeza da frota",
                 "Responsável por prover os recursos para a hidratação, aquecimento e entretenimento em cada viagem contratada quando solicitado",
-                "Responsável por criar, distribuir, coletar, catalogar, auditar e arquivar as fichas de controle de viagem (fichas de KM)",
+                "Responsável por gerenciar as fichas de controle de viagem", 
                 "Responsável pelo treinamento de check in e check out dos novos motoristas colaboradores",
                 "Suporte administrativo ao setor de manutenção com tarefas de coleta de dados sobre a frota",
                 "Suporte administrativo ao setor comercial com tarefas de agendamento de viagens tanto no sistema interno como em órgãos reguladores de transporte rodoviário (Artesp e ANTT)",
